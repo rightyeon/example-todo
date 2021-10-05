b01e41ba77a0867f7befda467882dd47597d83bf
yeon <bacchus.xd@gmail.com>
2021-10-05 14:12:30 +0900

    todo ex

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-const todoBtn   = document.querySelector('#addBtn');
-const todoText  = document.querySelector('#todoValue');
-const todoBox   = document.querySelector('#toDoBody');
+// 투두리스트 구현을 위한 변수 정의
+const todoBtn    = document.querySelector('#addBtn');
+const todoText   = document.querySelector('#todoValue');
+const todoBox    = document.querySelector('#toDoBody');
 const _TODOLIST  = 'toDoList';
+const isToDos    = localStorage.getItem(_TODOLIST);
 
-let todoArr = [];
+let todoArr      = [];
 
+// localStorage에 todo 가 있는지 확인
+if (isToDos !== null) {
+    const getToDos = JSON.parse(isToDos);
+    toDoArr = getToDos;
+    getToDos.forEach((item) => drawToDo(item.todo, item.done, item.id));
+}
+
+// 엔터키 입력시
 function enterHandler(e) {
     if (event.keyCode == 13) {
         toDoHandler(e);   
     }
 }
 
+// 할일 추가 시
 function toDoHandler(e) {
     e.preventDefault();
-    // 빈 텍스트라면 Input Focus || placeholder 대로 추가해주기
-    // 할일 목록 추가하기
+    // 빈 텍스트라면 placeholder 대로 추가해주자
     const todoValue = todoText.value; 
-    if (todoValue.length < 1) {
+    if (todoValue.trim().length < 1) {
+        todoText.value='';
         todoText.focus();
     } 
     else {
@@ -27,33 +38,29 @@ function toDoHandler(e) {
     }
 }
 
-function doneToDo(e) {
-    console.log('done');
-}
 
-function saveToDo() {  
-    localStorage.setItem(_TODOLIST, JSON.stringify(todoArr));
-}
 
-function addArr(text, id) { 
+// 할일을 배열에 넣어주기
+function addArr(text,done,id) {
     const obj = {
         todo : text,
         id   : id,
-        done : false,
+        done : (done)?done:false,
     }
     todoArr.push(obj);
 }
 
-function drawToDo(text) {
-    let id = todoArr.length+1;
-
-    addArr(text, id);
+// 할일 Create, Read
+function drawToDo(text,done,id) {
+    const UUID = (id)?id:Date.now();
+    addArr(text,done,UUID);
 
     let li = document.createElement("li");
     li.className = 'list'; 
-    li.setAttribute('id',id);
+    li.setAttribute('id',UUID);
 
     let btn = document.createElement('i');
+
     // 폰트어썸은 class 로 추가하지 말고 setAttribute 로 그려줘야함
     btn.setAttribute('class', 'far fa-trash-alt text-red-500');
     btn.addEventListener('click', deleteToDo);
@@ -65,9 +72,10 @@ function drawToDo(text) {
     let checkbox = document.createElement('input');
     checkbox.setAttribute('type','checkbox');
     checkbox.className = 'checkbox checkbox-primary checkbox-md mr-3';
-    label.appendChild(checkbox);
 
-    // append는 여러개 인자를 받을 수 있고, appendChild는 노드객체 1개만 받을 수 있음
+    if(done==true) checkbox.checked = true;
+
+    label.appendChild(checkbox);
 
     let span = document.createElement('span');
     span.innerText = text;
@@ -77,24 +85,32 @@ function drawToDo(text) {
 
 }
 
+// 할일 완료처리
+function doneToDo(e) {
+    const isCheck = e.target.parentElement.children[0].checked;
+    const id = +e.target.parentElement.parentElement.id;
+    todoArr.forEach((ele) => {
+        if (id == ele.id) {
+            ele.done = (isCheck)?true:false;
+        }
+    });
+    saveToDo();
+}
+
+// 할일 저장
+function saveToDo() {  
+    localStorage.setItem(_TODOLIST, JSON.stringify(todoArr));
+}
+
+// 할일 삭제
 function deleteToDo(e) {
     const removeTarget = e.target.parentElement;
     removeTarget.remove();
 
     todoArr = todoArr.filter((todo) => todo.id !== parseInt(removeTarget.id));
-    saveToDo();
-    // console.log(id);
+    saveToDo(); 
 }
 
 todoBtn.addEventListener('click',  toDoHandler);
 todoText.addEventListener('keydown', enterHandler);
- 
-
-const isToDos = localStorage.getItem(_TODOLIST);
-
-if (isToDos !== null) {
-    const getToDos = JSON.parse(isToDos);
-    toDoArr = getToDos;
-    getToDos.forEach((item) => drawToDo(item.todo));
-}
-
+  
